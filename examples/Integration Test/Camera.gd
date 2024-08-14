9bbc0d51148dbfd8ee393f0ab33b38d219776336
Chance Snow <git@chancesnow.me>
2024-08-14 17:52:18 -0500

    Stub out a demo world

---
+++ examples/Integration Test/Camera.gd
@@ -0,0 +1,1 @@
+extends InterpolatedCamera3D
---
+++ examples/Integration Test/world.gd
@@ -0,0 +1,9 @@
+@tool extends Node3D
+
+@onready var env = $Environment
+@onready var sun = $Sun
+@onready var camera = $Camera
+
+func _ready():
+	#camera.look_at($FocalPoint.basis)
+	pass
